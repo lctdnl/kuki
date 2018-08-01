c21631ea799bc0cd4f4bb77cb4272a17bd131d46
Daniel Lechthaler <lechthaler.d@gmail.com>
2018-08-01 14:12:22 +0200

    Add polyfil for object.assign

--- app/assets/javascripts/kuki/cookie_consent_init.js
+++ app/assets/javascripts/kuki/cookie_consent_init.js
@@ -1,3 +1,33 @@
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
+
 var default_options = {
   "palette": {
     "popup": {
@@ -28,7 +58,7 @@ init_kuki = function(options) {
       "link": i18n_data.learn_more_link,
     }
   }
-  
+
   consent_options = Object.assign(default_options, options, i18n);
   window.cookieconsent.initialise(consent_options)
 }
@@ -44,6 +74,5 @@ $(document).on('turbolinks:load', function () {
     e.preventDefault()
     Cookies.set('cookieconsent_status', 'allow')
     window.setTimeout(function () { window.location.reload() }, 100)
-  })  
+  })
 })
-
